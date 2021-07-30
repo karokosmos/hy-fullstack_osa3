858ed7b18b4bb46302b27a076bf3e67573b31028
Karo Kosmos <okava035@edu.xamk.fi>
2021-07-30 14:33:35 +0300

    Add MongoDB

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 const app = express()
+const Person = require('./models/person')
 
-app.use(cors())
+/*************************/
+// MIDDLEWARES
+/*************************/
 app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -14,44 +17,17 @@ morgan.token('body', (req, res) => {
   }
 })
 
-/*************************/
-// DATA
-/*************************/
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456"
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523"
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345"
-  },
-  {
-    id: 4,
-    name: "Mary Poppendick",
-    number: "39-23-6423122"
-  }
-]
-
-/*************************/
-// FUNCTIONS
-/*************************/
-const generateId = () => {
+/* const generateId = () => {
   return Math.floor(Math.random() * 1000)
-}
+} */
 
 /*************************/
 // GET
 /*************************/
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (req, res) => {
@@ -96,21 +72,20 @@ app.post('/api/persons', (req, res) => {
     })
   }
 
-  if (persons.find(person => person.name === body.name)) {
+  /* if (persons.find(person => person.name === body.name)) {
     return res.status(400).json({
       error: 'name must be unique'
     })
-  }
+  } */
 
-  const person = {
-    id: generateId(),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  }
+  })
 
-  persons = persons.concat(person)
-
-  res.json(person)
+  person.save().then(savedPerson => {
+    res.json(person)
+  })
 })
 
 /*************************/
@@ -138,7 +113,33 @@ app.post('/api/persons', (req, res) => {
 /*************************/
 // PORT
 /*************************/
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+/*************************/
+// DATA
+/*************************/
+/* let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456"
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523"
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345"
+  },
+  {
+    id: 4,
+    name: "Mary Poppendick",
+    number: "39-23-6423122"
+  }
+] */
